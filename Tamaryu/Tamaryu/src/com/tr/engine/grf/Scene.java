6c2b11f76e36ede819f68d9e7dd77d374dbb7da9
darkeye2 <admin@wassiluk.com>
2016-06-20 17:03:05 +0200

    Change-Id: Iff62dd54dd9627665eb8e8c25751acd6a3224005

--- Tamaryu/Tamaryu/src/com/tr/engine/grf/Scene.java
+++ Tamaryu/Tamaryu/src/com/tr/engine/grf/Scene.java
@@ -152,7 +152,6 @@ public class Scene extends JComponent implements ComponentListener {
 	 */
 	public void reorderComponents() {
 		// change component order depending on position
-		//test
 	}
 
 	public void updateFOComponents() {
