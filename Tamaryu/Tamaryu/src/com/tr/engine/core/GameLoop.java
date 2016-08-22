29547941eddb1cefc0ea9804b733435a5cb62322
-Nero <-Nero@10.31.0.130>
2016-08-22 13:48:33 +0200

    extra thread for update logic

--- Tamaryu/Tamaryu/src/com/tr/engine/core/GameLoop.java
+++ Tamaryu/Tamaryu/src/com/tr/engine/core/GameLoop.java
@@ -13,62 +13,62 @@ public class GameLoop {
 	private TRRenderContext rc;
 	private TRGameWindow gw;
 	private TRScene scene;
-	
-	//private TestTama2 t1= null;
+
+	// private TestTama2 t1= null;
 	private TRGLImageView t1 = null;
 	private TRGLImageView container = new TRGLImageView();
-	
-	//by florian
+
+	// by florian
 	private GameLooper gl;
-	
-	
-	public GameLoop(){
-		//by florian
+	private GameLooper ul;
+
+	public GameLoop() {
+		// by florian
 		gl = new GameLooper();
-		
-		rc = new TRGLRenderContext(800, 600);	//create new RenderContext for OpenGL (GL4ES3 profile!)
-		rc.setDebug(true);						//enable debug mode (debug console)
-		rc.init();								//initialize the context
-		
-		/* get game window for the context
+		ul = new GameLooper();
+
+		rc = new TRGLRenderContext(800, 600); // create new RenderContext for
+												// OpenGL (GL4ES3 profile!)
+		rc.setDebug(true); // enable debug mode (debug console)
+		rc.init(); // initialize the context
+
+		/*
+		 * get game window for the context
 		 * 
-		 * - window size
-		 * - full screen
-		 * - mouse visibility
-		 * - window titel
-		*/ 
+		 * - window size - full screen - mouse visibility - window titel
+		 */
 		gw = rc.createWindow();
-		
-		/* get scene for this context
+
+		/*
+		 * get scene for this context
 		 * 
-		 * - get / set FPS
-		 * - add renderable components
+		 * - get / set FPS - add renderable components
 		 */
 		scene = rc.getScene();
-		
-		//TestGLTama t1 = new TestGLTama();
+
+		// TestGLTama t1 = new TestGLTama();
 		TRImage img = new TRImage("dancing", "dancing1", "png", "/img", 0, 0, 0, 600, 600, 600, 600);
-		System.out.println("Image path: "+img.getFullFilePath());
+		System.out.println("Image path: " + img.getFullFilePath());
 		t1 = new TRGLImageView(img);
 		t1.setSize(300, 300);
-		//t1 = new TestTama2();
+		// t1 = new TestTama2();
 		t1.setRotation(0, 180, 0);
-		//t1.setPosition(0.5f, 0.5f, 5f);
+		// t1.setPosition(0.5f, 0.5f, 5f);
 		TestGLTamaEye t2 = new TestGLTamaEye();
 		t2.setName("test");
 		t1.addComponent(t2);
-		
+
 		TRGLLabel l = new TRGLLabel("TEST");
-		
+
 		container.setSize(300, 300);
 		container.addComponent(t1);
-		//container.setScale(0.5f);
-		
+		// container.setScale(0.5f);
+
 		scene.addComponent(container);
 		scene.addComponent(l);
 		System.out.println(t1.getComponentByName("test"));
-		
-		Thread t = new Thread(new Runnable(){
+
+		Thread t = new Thread(new Runnable() {
 
 			@Override
 			public void run() {
@@ -76,56 +76,68 @@ public class GameLoop {
 				int r = 0;
 				boolean forward = true;
 				boolean fr = true;
-				while(gl.getRunning()){
-					//move
+				while (gl.getRunning()) {
+					// move
 					container.setPosition(i, 0, 100);
-					if(forward){
-						i+=4;
-					}else{
-						i-=4;
+					if (forward) {
+						i += 4;
+					} else {
+						i -= 4;
 					}
-					if(i>=500f/container.getScale()){
+					if (i >= 500f / container.getScale()) {
 						forward = false;
-					}else if(i<=0){
+					} else if (i <= 0) {
 						forward = true;
 					}
-					
-					//rotate
+
+					// rotate
 					t1.setXRotation(r);
-					if(fr){
-						r+=2;
-					}else{
-						r-=2;
+					if (fr) {
+						r += 2;
+					} else {
+						r -= 2;
 					}
-					if(r>=20){
+					if (r >= 20) {
 						fr = false;
-					}else if(r<=0){
+					} else if (r <= 0) {
 						fr = true;
 					}
-					
-					//by florian
-					//Loop by GameLooper
+
+					// by florian
+					// Loop by GameLooper
 					gl.think(System.nanoTime());
 					/*
-					try {
-						Thread.sleep(33);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					*/
+					 * try { Thread.sleep(33); } catch (InterruptedException e)
+					 * { // TODO Auto-generated catch block e.printStackTrace();
+					 * }
+					 */
 				}
-			}});
-		t.start();
+			}
+		});
 		
-		//start game window
+
+		// by florian, based on the above, but empty
+		Thread tu = new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (ul.getRunning()) {
+					
+					// by florian
+					ul.think(System.nanoTime());
+				}
+			}
+		});
+		
+		t.start();
+		tu.start();
+
+		// start game window
 		System.out.println("Starting Window!");
 		gw.start();
 	}
-	
-	
-	
-	public static void main(String[] args){
+
+	public static void main(String[] args) {
 		new GameLoop();
 	}
 
