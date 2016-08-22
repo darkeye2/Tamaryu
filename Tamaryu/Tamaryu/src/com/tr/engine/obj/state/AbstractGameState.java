96cdb8962827e297ff879533eee2cac85cb2d9df
-Nero <-Nero@10.31.0.130>
2016-08-22 15:29:23 +0200

    sort algorithm for ArrayList in AbstractGameState small stuff

--- Tamaryu/Tamaryu/src/com/tr/engine/obj/state/AbstractGameState.java
+++ Tamaryu/Tamaryu/src/com/tr/engine/obj/state/AbstractGameState.java
@@ -3,10 +3,11 @@ package com.tr.engine.obj.state;
 import java.util.ArrayList;
 
 import com.tr.engine.gameobject.AbstractGameObject;
+import com.tr.engine.gameobject.*;
 
 public abstract class AbstractGameState {
 
-	protected ArrayList<AbstractGameObject> currentObject = new ArrayList<AbstractGameObject>();
+	protected ArrayList<AbstractGameObject> currentObjects = new ArrayList<AbstractGameObject>();
 
 	public void init() {
 
@@ -28,10 +29,39 @@ public abstract class AbstractGameState {
 
 	}
 
+	public ArrayList<AbstractGameObject> sortAL(ArrayList<AbstractGameObject> A) {
+		ArrayList<AbstractGameObject> nL = new ArrayList<AbstractGameObject>();
+
+		int s = A.size();
+
+		for (int i = 0; i < s - 1; i = i + 1) {
+			if (A.get(i) instanceof Zone) {
+				nL.add(A.get(i));
+			}
+		}
+		for (int i = 0; i < s - 1; i = i + 1) {
+			if (A.get(i) instanceof Decoration) {
+				nL.add(A.get(i));
+			}
+		}
+		for (int i = 0; i < s - 1; i = i + 1) {
+			if (A.get(i) instanceof Prop) {
+				nL.add(A.get(i));
+			}
+		}
+		for (int i = 0; i < s - 1; i = i + 1) {
+			if (A.get(i) instanceof Actor) {
+				nL.add(A.get(i));
+			}
+		}
+		return nL;
+	}
+
 	public void update() {
-
-		for (AbstractGameObject go : currentObject) {
-
+		currentObjects = sortAL(currentObjects);
+		
+		for (AbstractGameObject go : currentObjects) {
+			go.update();
 		}
 
 	}
