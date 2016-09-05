a898418862d9dfeb5fffa9d97bf00717663bf927
-Nero <-Nero@10.31.0.132>
2016-09-05 10:06:31 +0200

    zitzc

--- Tamaryu/Tamaryu/src/com/tr/engine/obj/state/GameStateFactory.java
+++ Tamaryu/Tamaryu/src/com/tr/engine/obj/state/GameStateFactory.java
@@ -3,10 +3,21 @@ package com.tr.engine.obj.state;
 public final class GameStateFactory {
 	
 	private static final int LoginStateID = 0;
-	private static final int MenuStateID = 1;
-	private static final int GameStateID = 2;
-	private static final int MiniGameStateID = 3;
+	private static final int IslandStateID = 1;
+	private static final int DragonProfileStateID = 2;
+	private static final int InteractionStateID = 3;
+	private static final int MiniGameStateID = 4;
+	private static final int ResultStateID = 5;
+	private static final int BreedingStateID = 6;
 	
+	/*
+	public static final int STARTMENUSTATE = 0;
+	public static final int GAMESTATE = 1;
+	public static final int LANGUAGESTATE = 2;
+	public static final int HELPSTATE = 3;
+	public static final int TESTSTATE = 4;
+	public static final int INVENTORYTESTSTATE = 5;
+	*/
 	
 	public static AbstractGameState create(int id){
 		
@@ -21,6 +32,15 @@ public final class GameStateFactory {
 			break;
 			
 		case 3:
+			break;
+
+		case 4:
+			break;
+			
+		case 5:
+			break;
+			
+		case 6:
 			break;
 		
 		}
